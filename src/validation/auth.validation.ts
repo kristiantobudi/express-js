d036cf9845de2625e386b2abcd349be2d3ad6c0f
Kristianto Budi Purwoko <kristiantobudipurwoko@gmail.com>
2024-08-23 22:31:00 +0700

    auth validation

--- src/validation/auth.validation.ts
+++ src/validation/auth.validation.ts
@@ -32,6 +32,7 @@ export const createSessionValidation = (payload: UserType) => {
 export const deleteSessionValidation = (payload: UserType) => {
     const schema = Joi.object({
         accessToken: Joi.string().required(),
+        refreshToken: Joi.string().required()
     })
     return schema.validate(payload)
 }
