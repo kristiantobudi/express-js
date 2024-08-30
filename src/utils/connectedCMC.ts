3151fa16f483ae89c7895558aeb2ba6df58fc5f6
Kristianto Budi Purwoko <kristiantobudipurwoko@gmail.com>
2024-08-30 20:50:26 +0700

    update BE

--- src/utils/connectedCMC.ts
+++ src/utils/connectedCMC.ts
@@ -1,16 +1,17 @@
+/* eslint-disable @typescript-eslint/return-await */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable eol-last */
 /* eslint-disable object-shorthand */
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import express from 'express'
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 const cmc_api_key = process.env.CMC_API_KEY
 const url_api = process.env.CMC_URL
 
-export const getListingsLatest = async () => {
+export const getListingsLatest = async (): Promise<any> => {
   try {
-    const response = await axios.get(`${url_api}/v1/cryptocurrency/listings/latest`, {
+    const response: AxiosResponse = await axios.get(`${url_api}/v1/cryptocurrency/listings/latest`, {
       headers: {
         'X-CMC_PRO_API_KEY': cmc_api_key
       }
@@ -19,6 +20,6 @@ export const getListingsLatest = async () => {
     return response.data
   } catch (error) {
     console.error('Error fetching CoinMarketCap data:', error)
-    throw error
+    return Promise.reject(error)
   }
 }
