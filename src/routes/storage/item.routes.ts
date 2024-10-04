57970748bd85246f742925730207e2b33dc1b942
Kristianto Budi Purwoko <kristiantobudipurwoko@gmail.com>
2024-10-04 16:37:11 +0700

    resolved api get item

--- src/routes/storage/item.routes.ts
+++ src/routes/storage/item.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import { createItem, getItem, deleteItem, updateItem } from '../../modules/storage/controller/itemController'
-import { requireAdmin, requireUser } from '../../middleware/auth'
+import { requireAdmin } from '../../middleware/auth'
 
 export const ItemRouter: Router = Router()
 
 ItemRouter.post('/v1/items', requireAdmin, createItem)
-ItemRouter.get('/v1/items', requireAdmin, getItem)
-ItemRouter.get('/v1/items/:id', requireAdmin, getItem)
+ItemRouter.get('/v1/items', getItem)
+ItemRouter.get('/v1/items/:id', getItem)
 ItemRouter.put('/v1/items/:id', requireAdmin, updateItem)
 ItemRouter.delete('/v1/items/:id', requireAdmin, deleteItem)
