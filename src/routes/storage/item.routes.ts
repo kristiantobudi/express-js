410d63b6dd61303bd5f31cd36324ad62b4e3ffbb
Kristianto Budi Purwoko <kristiantobudipurwoko@gmail.com>
2024-10-04 16:31:51 +0700

    update

--- src/routes/storage/item.routes.ts
+++ src/routes/storage/item.routes.ts
@@ -5,7 +5,7 @@ import { requireAdmin, requireUser } from '../../middleware/auth'
 export const ItemRouter: Router = Router()
 
 ItemRouter.post('/v1/items', requireAdmin, createItem)
-ItemRouter.get('/v1/items', requireUser, getItem)
-ItemRouter.get('/v1/items/:id', requireUser, getItem)
-ItemRouter.put('/v1/items/:id', requireUser, updateItem)
-ItemRouter.delete('/v1/items/:id', requireUser, deleteItem)
+ItemRouter.get('/v1/items', requireAdmin, getItem)
+ItemRouter.get('/v1/items/:id', requireAdmin, getItem)
+ItemRouter.put('/v1/items/:id', requireAdmin, updateItem)
+ItemRouter.delete('/v1/items/:id', requireAdmin, deleteItem)
