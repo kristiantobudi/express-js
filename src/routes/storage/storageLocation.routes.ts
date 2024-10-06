f919891723e9a57fe753a3950a364ebb6019079b
Kristianto Budi Purwoko <kristiantobudipurwoko@gmail.com>
2024-10-06 14:36:40 +0700

    update

--- src/routes/storage/storageLocation.routes.ts
+++ src/routes/storage/storageLocation.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { createStorage, deleteStorageLocation, getStorageLocation, updateStorageLocation } from '../../modules/storage/controller/storageLocationController'
+import { requireAdmin } from '../../middleware/auth'
 
 export const StorageLocationRouter: Router = Router()
 
-StorageLocationRouter.post('/v1/storage-locations', createStorage)
+StorageLocationRouter.post('/v1/storage-locations', requireAdmin, createStorage)
 StorageLocationRouter.get('/v1/storage-locations', getStorageLocation)
 StorageLocationRouter.get('/v1/storage-locations/:id', getStorageLocation)
 StorageLocationRouter.put('/v1/storage-locations/:id', updateStorageLocation)
